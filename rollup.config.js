01e9beea77d8a57a44c1c2fd9183d425d4709941
Jia Huang <iamfat@gmail.com>
2020-03-29 23:54:10 +0800

    清理不必要的文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default [
