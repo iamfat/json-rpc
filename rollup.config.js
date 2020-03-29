cb6f0eec5770b607ef3cf2a860a3d38618ce8327
Jia Huang <iamfat@gmail.com>
2020-03-29 23:41:02 +0800

    force json-rpc use external nanoid and hash-sum

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,27 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            format: 'cjs',
-            dir: 'lib',
-        },
-    ],
-    plugins: [typescript(), resolve(), commonjs(), terser()],
-};
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                format: 'cjs',
+                dir: 'lib',
+            },
+        ],
+        external: ['hash-sum', 'nanoid/non-secure'],
+        plugins: [typescript({ declaration: true, outDir: 'lib' }), resolve(), commonjs()],
+    },
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                format: 'es',
+                file: 'lib/index.es.js',
+            },
+        ],
+        external: ['hash-sum', 'nanoid/non-secure'],
+        plugins: [typescript(), resolve(), commonjs()],
+    },
+];
