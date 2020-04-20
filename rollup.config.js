1a718e573b2cf3c733a44222ced73925ea18f493
Jia Huang <iamfat@gmail.com>
2020-04-20 13:52:33 +0800

    add sourcemap and bump to 1.0.6

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
 export default [
     {
@@ -9,9 +10,10 @@ export default [
             {
                 format: 'cjs',
                 dir: 'lib',
+                sourcemap: true
             },
         ],
         external: ['hash-sum', 'nanoid/non-secure'],
-        plugins: [typescript({ declaration: true, outDir: 'lib' }), resolve(), commonjs()],
+        plugins: [typescript({ declaration: true, outDir: 'lib' }), resolve(), commonjs(), terser()],
     }
 ];
